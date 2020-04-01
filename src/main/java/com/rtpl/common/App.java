6920a2e292041b85abe2f0e65b33c79ed008b4ab
neha <nehatyagi2516@gmail.com>
2020-04-01 13:49:52 +0530

    first commit

---
+++ src/main/java/com/rtpl/common/App.java
@@ -0,0 +1,25 @@
+package com.rtpl.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+    {
+    	ApplicationContext context = 
+    	 new ClassPathXmlApplicationContext(new String[] {"Spring-Customer.xml"});
+
+    	CustomerService custA = (CustomerService)context.getBean("customerService");
+    	custA.setMessage("Message by custA");
+    	System.out.println("Message : " + custA.getMessage());
+    	
+    	//retrieve it again
+    	CustomerService custB = (CustomerService)context.getBean("customerService");
+    	System.out.println("Message : " + custB.getMessage());
+    }
+}
---
+++ src/main/java/com/rtpl/common/CustomerService.java
@@ -0,0 +1,13 @@
+package com.rtpl.common;
+
+public class CustomerService {
+String message;
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
